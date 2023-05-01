5478929075fb3b52f9f366c6b9a2907fbba1ea46
ZahraaTayyar <zahraatayyar@gmail.com>
2023-05-01 23:39:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TopBar from './topbar/TopBar';
+
+function App() {
+  return (
+    <div>
+      <TopBar />
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../topbar/TopBar.css";
+
+function TopBar() {
+  return <div className="top">TopBar</div>;
+}
+
+export default TopBar;
