b32964846935c7a6b08f463ad2e0047a905d34eb
ZahraaTayyar <zahraatayyar@gmail.com>
2023-05-03 14:22:42 +0100

    Write page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import Homepage from "./components/Homepage";
-import Single from "./components/Single";
+// import Single from "./components/Single";
 import TopBar from "./components/TopBar";
+import Write from "./components/Write";
 
 function App() {
   return (
     <div>
       <TopBar />
-      <Single />
+      <Write />
     </div>
   );
 }
